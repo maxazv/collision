248f5a944117b8d6c3afbb93ec5b1b05f9f14b4a
maxazvd <azevedomaximilian@gmail.com>
2021-09-16 17:10:51 +0200

    did not test...

--- sketch.js
+++ sketch.js
@@ -47,6 +47,61 @@ function Particle(x, y, r){
     }
 }
 
+function Grid(size){
+    this.size = size;
+    this.grids = []; // [[edges], content...]
+
+    this.init = function(){
+        stepX = innerWidth/this.size;
+        stepY = innerHeight/this.size;
+        for(var i = 0; i<this.size; i++){
+            for(var j = 0; j<this.size; j++){
+                grid = []
+
+                a = (stepX/2);
+                ctrX = stepX*i - a;
+                ctrY = stepY*j - a;
+                
+                edges = [
+                    [ctrX-a, ctrY-a],
+                    [ctrX+a, ctrY-a],
+                    [ctrX+a, ctrY+a],
+                    [ctrX-a, ctrY+a]
+                ];
+
+                grid.push(edges);
+                this.grids.push(grid);
+            }
+        }
+    }
+    this.init();
+
+    this.in_grid = function(edges, pos){
+        if (pos[0] > edges[0][0] && pos[1] > edges[0][1] &&
+            pos[0] < edges[1][0] && pos[1] > edges[1][1] &&
+            pos[0] < edges[2][0] && pos[1] < edges[2][1] &&
+            pos[0] > edges[3][0] && pos[1] < edges[3][1]){
+                return true;
+            }
+        return false;
+    }
+    this.get_grid = function(pos){
+        for(var i = 0; i<this.grids.length; i++){
+            edges = this.grids[i][0]
+            if(this.in_grid(edges, pos)){
+                return i;
+            }
+        }
+    }
+    this.add_objects = function(objs){
+        for(var i = 0; i<objs.length; i++){
+            pos = objs[i].pos;
+            idx = this.get_grid(pos);
+            this.grids[idx].push(this.objs[i]);
+        }
+    }
+}
+
 // ---declare
 
 var particles = []
@@ -56,10 +111,32 @@ for(var i = 0; i<10; i++){
     particles.push(new Particle(x, y, 20));
 }
 
-
 // ---funcs
 
+function dist(v1, v2){
+    dx = v1[0] - v2[0];
+    dy = v1[1] - v2[1];
+    var sum = Math.pow(dx, 2) + Math.pow(dy, 2);
+    
+    return Math.sqrt(sum);
+}
+function angle(v1, v2){
 
+}
+function collision_simple(objs){
+    for(var i = 0; i<objs.length; i++){
+        for(var j = 0; j<objs.length; j++){
+            if(i == j){ continue; }
+
+            radsum = objs[i].r + objs[j].r
+            if(dist(objs[i].pos, objs[j].pos) < radsum){
+
+                aoc = angle(objs[i].pos, objs[j].pos)
+
+            }
+        }
+    }
+}
 
 // ---animate
 
