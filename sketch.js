a0125c86c5680a90de40678f3ce7241e1cfa2373
maxazvd <azevedomaximilian@gmail.com>
2021-09-18 16:38:11 +0200

    start

--- sketch.js
+++ sketch.js
@@ -13,10 +13,10 @@ pop = 10
 
 // ---classes
 function Particle(x, y, r){
-    this.pos = [x, y]
+    this.pos = Vector2D(x, y)
 
-    this.vel = [2, 0];
-    this.acc = [0, 0];
+    this.vel = Vector2D(0, 0);
+    this.acc = Vector2D(0, 0);
     this.m = randint(0.6, 1.2);
 
     this.r = r*this.m;
@@ -27,7 +27,7 @@ function Particle(x, y, r){
         c.beginPath();
         c.strokeStyle = 'rgba(255, 255, 255, 0.75)';
         c.fillStyle = 'rgba(255, 255, 255, 0.75)';
-        c.arc(this.pos[0], this.pos[1], this.r, 0, Math.PI*2, false);
+        c.arc(this.pos.x, this.pos.y, this.r, 0, Math.PI*2, false);
         c.stroke();
         c.fill();
         this.glow(47, 23, 0.4*this.r*0.025);
@@ -38,44 +38,41 @@ function Particle(x, y, r){
             opacity = 0.75/(i*glow);
             rgb = "rgba(220, 190, 230, " + String(opacity) + ")";
             c.fillStyle = rgb;
-            c.arc(this.pos[0], this.pos[1], this.r*(i*size), 0, Math.PI*2, false);
+            c.arc(this.pos.x, this.pos.y, this.r*(i*size), 0, Math.PI*2, false);
             c.fill();
         }
     }
 
     this.apply_force = function(force){
-        this.acc[0] += force[0]/this.m;
-        this.acc[1] += force[1]/this.m;
+        force.div(this.m)
+        this.acc.add(force);
     }
     this.update = function(){
-        this.vel[0] += this.acc[0];
-        this.vel[1] += this.acc[1];
-        this.acc[0] = 0;
-        this.acc[1] = 0;
+        this.vel.add(this.acc);
+        this.pos.add(vel);
 
-        this.pos[0] += this.vel[0];
-        this.pos[1] += this.vel[1];
+        this.acc.mult(0);
     }
 
     this.edge = function(){
-        if(this.pos[0]+this.r > w){
-            this.pos[0] = w - this.r;
-            this.vel[0] *= -1;
+        if(this.pos.x+this.r > w){
+            this.pos.x = w - this.r;
+            this.vel.x.mult(-1);
             return true;
         }
-        if(this.pos[0]-this.r < 0){
-            this.pos[0] = this.r;
-            this.vel[0] *= -1;
+        if(this.pos.x-this.r < 0){
+            this.pos.x = this.r;
+            this.vel.x.mult(-1);
             return true;
         }
-        if(this.pos[1]+this.r > h){
-            this.pos[1] = h - this.r;
-            this.vel[1] *= -1;
+        if(this.pos.y+this.r > h){
+            this.pos.y = h - this.r;
+            this.vel.y.mult(-1);
             return true;
         }
-        if(this.pos[1]-this.r < 0){
-            this.pos[1] = this.r;
-            this.vel[1] *= -1;
+        if(this.pos.y-this.r < 0){
+            this.pos.y = this.r;
+            this.vel.y.mult(-1);
             return true;
         }
         return false;
@@ -84,39 +81,24 @@ function Particle(x, y, r){
 
 
 // ---funcs
-function vec_mag(v){
-    var sum = Math.pow(v[0], 2) + Math.pow(v[1], 2);
-    return Math.sqrt(sum);
-}
-function normalise(v){
-    mag = vec_mag(v);
-    return [v[0]/mag, v[1]/mag]
-}
-function dist(v1, v2){
-    dx = v1[0] - v2[0];
-    dy = v1[1] - v2[1];
-    var sum = Math.pow(dx, 2) + Math.pow(dy, 2);
-    
-    return Math.sqrt(sum);
-}
 function inv_vec(v1, v2){
-    dx = v2[0] - v1[0];
-    dy = v2[1] - v1[1];
-    repell_force = 0.1;
+    inv = Vector2D.sub(v2, v1);
 
-    return [dx*repell_force, dy*repell_force];
+    return ;
 }
 function collision_simple(objs, j){
     for(var i = 0; i<objs.length; i++){
         if(i == j || this.marked){ continue; }
 
         radsum = objs[i].r + objs[j].r;
-        if(dist(objs[i].pos, objs[j].pos) <= radsum){
+        if(objs[i].pos.dist(objs[j].pos) <= radsum){
 
-            inv_aoc = inv_vec(objs[i].pos, objs[j].pos);
+            aoc = Vector2D.sub(objs[j].pos, objs[i].pos);
+            repell_force = 0.1;
+            aoc.mult(repell_force);
             objs[j].apply_force(inv_aoc)
-            inv_aoc[0] *= -1
-            inv_aoc[1] *= -1
+            aoc.mult(-1);
+
             objs[i].apply_force(inv_aoc)
 
             objs[j].marked = true;
@@ -142,7 +124,7 @@ em_count = 4;
 // ---animate
 function animate(){
     requestAnimationFrame(animate);
-    c.fillStyle = 'rgba(8, 2, 18, 0.4)'
+    c.fillStyle = 'rgba(8, 4, 18, 0.4)'
     c.fillRect(0, 0, w, h)
 
     for(var i = 0; i<particles.length; i++){
@@ -152,9 +134,9 @@ function animate(){
         if(particles[i].edge()){
             vel = particles[i].vel
             friction_force = -1
-            friction = normalise(vel)
-            friction[0] *= friction_force
-            friction[1] *= friction_force
+            friction = vel.normalise();
+
+            friction.mult(friction_force);
             particles[i].apply_force(friction)
         }
         particles[i].draw();
--- vector.js
+++ vector.js
@@ -1,17 +1,39 @@
-function Vector2D(x, y){
-    this.x = x;
-    this.y = y;
+class Vector2D{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
 
-    this.add = function(vector){
+    add(vector){
         this.x += vector.x;
         this.y += vector.y;
     }
-    this.sub = function(vector){
+    sub(vector){
         this.x -= vector.x;
         this.y -= vector.y;
     }
-    this.mult = function(scalar){
+    mult(scalar){
         this.x *= scalar;
         this.y *= scalar;
+    }
+    div(scalar){
+        this.x /= scalar;
+        this.y /= scalar;
+    }
+    mag(){
+        sqr_sum = this.x*this.x + this.y*this.y;
+        return Math.sqrt(sqr_sum);
+    }
+    dist(vector){
+        dir = vector.sub(this);
+        return dir.mag();
+    }
+    normalise(){
+        magn = this.mag();
+        return Vector2D(this.x/magn, this.y/magn);
+    }
+
+    static sub(v1, v2){
+        vec = Vector2D(v1.x-v2.x, v1.y-v2.y);
     }
 }
